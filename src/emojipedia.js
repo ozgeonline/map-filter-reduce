576de0daf1bd1305118af99a1db6056f862970ad
ozgeonline <nisroc.414@hotmail.com>
2023-08-09 09:51:47 +0300

    test

---
+++ src/emojipedia.js
@@ -0,0 +1,44 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Nerd emoji 🤓 is a face with big glasses, an awkward smile and buck teeth. It's often used in a humorous or ironic way to describe nerds or to express how smart you are. It represents the stereotype of the nerd, an intelligent but oddly dressed person with social deficits. "
+  
+  },
+  {
+    id: 5,
+    emoji: "🤩",
+    name: "Smirking Face With Starry Eyes",
+    meaning:"Overwhelmed with excitement, the 🤩 emoji shows a face with a wide, open smile and stars for eyes, as if seeing a loved one. The color of the stars varies by platform, but is often gold or red. This emoji indicates that someone or something is amazing, fascinating, impressive, or exciting. "
+  },
+  {
+    id: 6,
+    emoji: "🧐",
+    name: " Face With Monocle",
+    meaning:"With its monocle, the 🧐 emoji represents a face that was considered a status symbol in the 19th century. It shows furrowed eyebrows and a slightly raised head, as if examining something closely."
+  }
+];
+export default emojipedia;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import emojipedia from './emojipedia';
 import reportWebVitals from './reportWebVitals';
+
+
+//! ----- MAP -----
+var Numbers = [3,56,2,48,5];
+const mapNumbers = Numbers.map(function(x){
+  return x *10;
+})
+console.log("new numbers",mapNumbers)
+
+//! ----- FILTER -----
+// var filterNumber = [];
+// Numbers.forEach(function(num){
+//   if(num>10){
+//     filterNumber.push(num)
+//   }
+// })
+const filterNumber = Numbers.filter(function(num){
+  return num > 10;
+})
+console.log("filter numbers",filterNumber);
+
+//! ----- REDUCE -----
+// var newNumber = 0;
+// Numbers.forEach(function(currentNumber){
+//   newNumber += currentNumber;
+// })
+let newNumber= Numbers.reduce(function(accumulatedValue, currentValue){
+  console.log("accumulator = " + accumulatedValue);
+  console.log("currentNumber = " + currentValue);
+  return accumulatedValue + currentValue
+})
+console.log("reduce", newNumber);
+
+//! ----- FIND -----
+const findNumber = Numbers.find(function(num){
+  return num > 10;
+})
+console.log("findNumber = " + findNumber)
+
+//! -----FINDINDEX -----
+const indexFindNumber = Numbers.findIndex((num)=>{return (num > 10)})
+console.log("findIndex = " + indexFindNumber);
+
+//! -----emoji-----
+const emojiMap = emojipedia.map(function (emojiEntry) {
+  return emojiEntry.meaning.substring(0,100)+"...";
+})
+console.log(emojiMap);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
